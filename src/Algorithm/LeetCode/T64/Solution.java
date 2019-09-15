653212ef6057524016c037f6183077c44262e860
王飞 <32962465+wangfei910@users.noreply.github.com>
2019-09-15 16:04:46 +0800

    Update Solution.java

--- src/Algorithm/LeetCode/T64/Solution.java
+++ src/Algorithm/LeetCode/T64/Solution.java
@@ -40,6 +40,26 @@ public class Solution {
         return memo[row][col];
     }
 
+    public static int minPathSum2(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0)
+            return 0;
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = dp[j];
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1];
+                } else {
+                    dp[i] = Math.min(dp[j], dp[j - 1]);
+                }
+                dp[j] += grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
+    
     /**
      * 主函数
      *
